223c292022868c03cc4f9d13daf39e049fb89fb7
artattack-fans <contact@artattack.fans>
2024-07-19 16:24:58 -0400

    qr hunt for art attack II

--- js/valid-qrs.js
+++ js/valid-qrs.js
@@ -7,9 +7,41 @@
 
 const validQRs = {
 	'1': {}, // test page
+
+	// QR 1 - CITY //
+	"0d0f6c": {
+		title: "Welcome to the HUNT",
+		hint: "There are many QR codes hidden throughout ART ATTACK II: PRUNE'S PLACE\n\nCollect all the words, and see what they say.\n\nCan you discover them all? And win ART ATTACK II: PRUNE'S PLACE.\n\nHINT: Your next objective is to discover the way of water. Don't forget to look up. Or down. I'm not your mom."
+	},
+
+	// QR 2 - SEWER
+	"a7bb63": {
+		title: "Take the next step",
+		hint: "The hang time for these things are enormous. Some call HIMESH a fashionista, and this is one of his favorite places."
+	},
+
+	// QR 3 - CLOSET
+	"2a54dc": {
+		title: "Hang ten",
+		hint: "I don't mean to ICE you out with this next hint."
+	},
+
+	// QR 4 - ICE
+	"1425e7": {
+		title: "It's Frosty in Here Wenches",
+		hint: "Now look to nature to find yourself. Turn over a new leaf, as they say."
+	},
+
+	// QR 5 - FOREST //
 	'5a22ff': {
-		title: 'Little goon',
-		hint: 'find her somewhere soon'
+		title: 'Bristled',
+		hint: 'Find the Queen of ART ATTACK II\'s home and you will be set free.'
+	},
+
+	// QR 6 - PRUNES PLACE
+	"92f089": {
+		title: 'Home Stretch',
+		hint: 'You\'ve completed the HUNT, but can you complete the SECRET PHRASE. Once done - create a piece of art corresponding to the SECRET PHRASE. Write the SECRET PHRASE, the TIME OF COMPLETION, and the AUTHORS on the back. We\'ll be watching.'
 	},
 
 	// throw em off our trail
