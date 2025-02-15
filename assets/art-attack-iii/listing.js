7abb407b56121e7e581a4cff17c5e21692424f8e
artattack-fans <contact@artattack.fans>
2025-02-15 17:24:01 -0500

    aaiii: tweak ordering

--- assets/art-attack-iii/listing.js
+++ assets/art-attack-iii/listing.js
@@ -1,13 +1,6 @@
 const GALLERY_LISTING = `
 PREDATOR.gif
 PREY.gif
-scene-1.png
-scene-2.png
-scene-3.png
-scene-4.png
-scene-5.png
-scene-6.png
-scene-7.jpeg
 scene-8.jpeg
 scene-9.jpeg
 scene-10.jpeg
@@ -15,6 +8,13 @@ scene-11.jpeg
 scene-12.jpeg
 scene-13.jpeg
 scene-14.jpeg
+scene-1.png
+scene-2.png
+scene-3.png
+scene-4.png
+scene-5.png
+scene-6.png
+scene-7.jpeg
 aa-iii-logo-text.png
 ART ATTACK III-TWILIGHT ZONE-TEASER.png
 `;
