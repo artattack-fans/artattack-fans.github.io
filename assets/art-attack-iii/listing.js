ed451b6eb0684eba17c2348e5a3b3843e1c24236
artattack-fans <contact@artattack.fans>
2025-02-15 17:22:52 -0500

    aaiii: finally developed our camara lol

--- assets/art-attack-iii/listing.js
+++ assets/art-attack-iii/listing.js
@@ -7,6 +7,14 @@ scene-3.png
 scene-4.png
 scene-5.png
 scene-6.png
+scene-7.jpeg
+scene-8.jpeg
+scene-9.jpeg
+scene-10.jpeg
+scene-11.jpeg
+scene-12.jpeg
+scene-13.jpeg
+scene-14.jpeg
 aa-iii-logo-text.png
 ART ATTACK III-TWILIGHT ZONE-TEASER.png
 `;
