b7ccaec6d46f07c4adfd42ed5efb8ff5ce9bad77
artattack-fans <contact@artattack.fans>
2025-06-16 01:11:20 -0400

    aav: submission link; gallery prep

---
+++ assets/art-attack-v/listing.js
@@ -0,0 +1,10 @@
+// const SPECIFIC_GALLERY_LISTING = `
+//
+// `.trim().split('\n').map(el => 'gallery/' + el).join('\n');
+
+const GALLERY_LISTING = `
+ART ATTACK V-HOTWHEELS DERBY-PROMO.png
+ART_ATTACK_V-HOTWHEELS_DERBY.png
+`;
+
+const getPrefix = () => "/assets/art-attack-v/";
