279f36a11bc8dbe63599b51ca848bb9a78b10218
artattack-fans <contact@artattack.fans>
2025-07-06 22:31:49 -0400

    aav: inspo

--- assets/art-attack-v/listing.js
+++ assets/art-attack-v/listing.js
@@ -3,6 +3,13 @@
 // `.trim().split('\n').map(el => 'gallery/' + el).join('\n');
 
 const GALLERY_LISTING = `
+slides/AA_SLIDE_FIVE.png
+slides/AA_SLIDE_FOUR.png
+slides/AA_SLIDE_ONE.png
+slides/AA_SLIDE_SIX.png
+slides/AA_SLIDE_SEVEN.png
+slides/AA_SLIDE_THREE.png
+slides/AA_SLIDE_TWO.png
 ART ATTACK V-HOTWHEELS DERBY-PROMO.png
 ART_ATTACK_V-HOTWHEELS_DERBY.png
 `;
