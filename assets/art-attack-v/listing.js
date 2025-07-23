506d26a8c0932c31c15ed6978015ea33e6c26953
artattack-fans <contact@artattack.fans>
2025-07-23 00:32:25 -0400

    aav: gallery 🏎️

--- assets/art-attack-v/listing.js
+++ assets/art-attack-v/listing.js
@@ -3,6 +3,16 @@
 // `.trim().split('\n').map(el => 'gallery/' + el).join('\n');
 
 const GALLERY_LISTING = `
+scenes/scene-0.jpeg
+scenes/scene-8.jpeg
+scenes/scene-5.jpeg
+scenes/scene-6.jpeg
+scenes/scene-7.jpeg
+scenes/scene-4.jpeg
+scenes/scene-1.jpeg
+scenes/scene-2.jpeg
+scenes/scene-3.jpeg
+KIRBY-FRIEND-PICNIC.jpg
 slides/AA_SLIDE_FIVE.png
 slides/AA_SLIDE_FOUR.png
 slides/AA_SLIDE_ONE.png
