381e109390ae5f1ad07f35829062fbe91a4437fc
artattack-fans <contact@artattack.fans>
2026-04-15 22:35:52 -0400

    aavi: choose the right font
    make the timeline spicieri

--- assets/art-attack-vi/timeline.js
+++ assets/art-attack-vi/timeline.js
@@ -6,24 +6,44 @@ const startMonth = 0; // January
 const endMonth = 3;   // April
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+/**
+ *
+ * @param eType
+ * @param opts? {{innerText?: string, classes?: string[]}}
+ * @returns {*}
+ */
+const createE = (eType, opts) => {
+    const e = document.createElement(eType);
+    if (opts.innerText)
+        e.innerText = opts.innerText;
+
+    if (opts.classes && opts.classes.length > 0)
+        opts.classes.forEach(c => e.classList.add(c))
+
+    return e;
+}
+
+const CYCLE = ['🤖', '🗿', '⚡️', '🦖', '🚀', '🪨', '💡', '🌲', '🏎️', '🕯️', '💾', '🌝']
 const appendDates = wrapper => {
     for (let y = startYear; y <= endYear; y++) {
         for (let m = startMonth; m < 12; m++) {
             if (y === endYear && m > endMonth)
                 break;
 
-            const monthItem = document.createElement('div');
-            monthItem.classList.add('month-item');
-            const monthLabel = document.createElement('span');
-            monthLabel.classList.add('month-label');
-            monthLabel.innerText = months[m];
-            const yearLabel = document.createElement('span');
-            yearLabel.classList.add('year-label');
-            yearLabel.innerText = y;
+            const monthItem = createE('div', {classes: ['month-item']});
+            const monthLabel = createE('span', {
+                classes: ['month-label'],
+                innerText: months[m]
+            });
+            const yearLabel = createE('span', {
+                classes: ['year-label'],
+                innerText: y
+            });
             monthItem.appendChild(monthLabel);
             monthItem.appendChild(yearLabel);
 
             wrapper.appendChild(monthItem);
+            wrapper.appendChild(createE('div', {innerText: CYCLE[m], classes: ['month-item', 'short']}));
         }
     }
 }
